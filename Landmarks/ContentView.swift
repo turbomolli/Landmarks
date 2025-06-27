457e626660b67b29f45faf69e954ebe934701436
Markus Lehtonen <makkeli@gmail.com>
2025-06-27 03:59:43 +0300

    testing colors

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -43,12 +43,12 @@ struct ContentView: View {
            
         }
         .background(MeshGradient(width: 3, height: 3, points: [
-                        [0.0, 0.0], [0.5, 0], [1.0, 0.0],
-                        [0.0, 0.5], [0.9, 0.5], [1.0, 0.5],
+                        [0.0, 0.0], [0.5, 0.0], [1.0, 0.0],
+                        [0.0, 0.5], [0.5, 0.5], [1.0, 0.5],
                         [0.0, 1.0], [0.5, 1.0], [1.0, 1.0]
                     ], colors: [
-                        .red, .purple, .indigo,
-                        .orange, .blue, .blue,
+                        .red, .white, .indigo,
+                        .orange, .teal, .blue,
                         .yellow, .green, .mint
                     ],
                                  
